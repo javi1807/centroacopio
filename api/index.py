08fa7f617f6df313007dd9968cc1cc8384916151
Javier <javier.tarrillo@unas.edu.pe>
2025-12-16 14:10:37 -0500

    Espero que ya

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add the 'backend' directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), '../backend'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../backend'))
 
 from django.core.wsgi import get_wsgi_application
 
